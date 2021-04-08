36afe2fa9c014cb15545bdeea7bf066903afb207
Nguyễn Minh Châu <61772618+minhchau97@users.noreply.github.com>
2021-04-08 20:51:50 +0700

    Update my_javascripts.js

--- my_javascripts.js
+++ my_javascripts.js
@@ -67,7 +67,7 @@ window.history.replaceState({}, document.title, clean_uri);
 }
 var uri = window.location.toString();
 if (uri.indexOf("&amp;m=1","&amp;m=1") > 0) {
-var clean_uri = uri.substring(0, uri.indexOf("&m=1"));
+var clean_uri = uri.substring(0, uri.indexOf("&amp;m=1"));
 window.history.replaceState({}, document.title, clean_uri);
 }
 var uri = window.location.toString();
