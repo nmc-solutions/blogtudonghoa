cb3d4cb468fdc1d9fac91735ae0d258de290ed35
Nguyễn Minh Châu <61772618+minhchau97@users.noreply.github.com>
2021-04-08 20:47:10 +0700

    Update my_javascripts.js

--- my_javascripts.js
+++ my_javascripts.js
@@ -1,86 +1,58 @@
-//FACEBOOK
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
 
+//replace image comment
 
-//SITEMAP
-var text_month = [, "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
-var no_image_url = "http://1.bp.blogspot.com/-eAeO-DYJDws/Vkqtj4HFBFI/AAAAAAAAB0o/Q5OLsyONXM0/s1600-r/nth.png";
+var oldSrc = &#39;//resources.blogblog.com/img/blank.gif&#39;; //Default blogger image link
+var newSrc = &#39;//4.bp.blogspot.com/-bixp2dJnPTA/YG0ybBAYcrI/AAAAAAAAEec/Oa6j_F309e8Dib7se_TRn5ZdXg8b_edtgCK4BGAYYCw/w80/user_btdh.png&#39;; //New image link
+$(&#39;img[src=&quot;&#39; + oldSrc + &#39;&quot;]&#39;).attr(&#39;src&#39;, newSrc);
+$(&#39;span[style=&quot;&#39; + oldSrc_1 + &#39;&quot;]&#39;).attr(&#39;src&#39;, newSrc_1);
 
-           var static_page_text = $.trim($('.static_page .post-body').text());
-if (static_page_text === "[sitemap]") {
-    var postbody = $('.static_page .post-body');
-    $.ajax({
-        url: "/feeds/posts/default?alt=json-in-script",
-        type: 'get',
-        dataType: "jsonp",
-        success: function(dataZ) {
-            var blogLabels = [];
-            for (var t = 0; t < dataZ.feed.category.length; t++) {
-                blogLabels.push(dataZ.feed.category[t].term)
-            }
-            var blogLabels = blogLabels.join('/');
-            postbody.html('<div class="siteLabel"></div>');
-            $('.static_page .post-body .siteLabel').text(blogLabels);
-            var splabel = $(".siteLabel").text().split("/");
-            var splabels = "";
-            for (get = 0; get < splabel.length; ++get) {
-                splabels += "<span>" + splabel[get] + "</span>"
-            }
-            $(".siteLabel").html(splabels);
-            $('.siteLabel span').each(function() {
-                var mapLabel = $(this);
-                var mapLabel_text = $(this).text();
-                $.ajax({
-                    url: "/feeds/posts/default/-/" + mapLabel_text + "?alt=json-in-script",
-                    type: 'get',
-                    dataType: "jsonp",
-                    success: function(data) {
-                        var posturl = "";
-                        var htmlcode = '<div class="mapa">';
-                        for (var i = 0; i < data.feed.entry.length; i++) {
-                            for (var j = 0; j < data.feed.entry[i].link.length; j++) {
-                                if (data.feed.entry[i].link[j].rel == "alternate") {
-                                    posturl = data.feed.entry[i].link[j].href;
-                                    break
-                                }
-                            }
-                            var posttitle = data.feed.entry[i].title.$t;
-                            var author = data.feed.entry[i].author[0].name.$t;
-                            var get_date = data.feed.entry[i].published.$t,
-                                year = get_date.substring(0, 4),
-                                month = get_date.substring(5, 7),
-                                day = get_date.substring(8, 10),
-                                date = text_month[parseInt(month, 10)] + ' ' + day + ', ' + year;
-                            var tag = data.feed.entry[i].category[0].term;
-                            var content = data.feed.entry[i].content.$t;
-                            var $content = $('<div>').html(content);
-                            var src2 = data.feed.entry[i].media$thumbnail.url;
-                            htmlcode += '<div class="mapapost"><div class="map-thumb"><div class="map-img"><a href="' + posturl + '" style="background:url(' + src2 + ') no-repeat center center;background-size: cover"/></div></div><h3 class="wrp-titulo"><a href="' + posturl + '">' + posttitle + '</a></h3><div class="map-meta"><span class="p-author">' + '</span><span class="p-date">' + date + '</span></div></div>'
-                        }
-                        htmlcode += '</div>';
-                        mapLabel.replaceWith('<div class="mapasite"><h2>' + mapLabel_text + '<span class="botao"><i class="fa fa-plus-circle"></i></span></h2>' + htmlcode + '</div>');
-                        $(document).on('click', '.mapasite h2', function() {
-                            $(this).parent('.mapasite').addClass('active');
-                            $(this).find('.botao .fa').removeClass('fa-plus-circle').addClass('fa-minus-circle');
-                        });
-                        $(document).on('click', '.mapasite.active h2', function() {
-                            $(this).parent('.mapasite').removeClass('active');
-                            $(this).find('.botao .fa').addClass('fa-plus-circle').removeClass('fa-minus-circle');
-                        });
-                    }
-                });
-            });
-        }
-    });
+  
+//Anti Copy and View Source
+  
+ var options = {
+  enableAlert: true, // Set this false to deactivate alert
+  alertText: &quot;Xin lỗi, Blog đã tắt chức năng này !&quot; // Customize this to change alert text
 }
+   // To disable right click
+document.addEventListener(&quot;contextmenu&quot;, function (e) {
+  e.preventDefault();
+  if(options.enableAlert) {
+    alert(options.alertText);
+  }
+});
 
-
+   // To disable F12 options
+document.onkeypress = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {
+alert(options.alertText);
+return false;
+}
+}
+document.onmousedown = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {
+alert(options.alertText);
+return false;
+}
+}
+document.onkeydown = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {
+alert(options.alertText);
+return false;
+}
+}
+// To To Disable ctrl+c, ctrl+u
+jQuery(document).ready(function($){
+$(document).keydown(function(event) {
+var pressedKey = String.fromCharCode(event.keyCode).toLowerCase();
+if (event.ctrlKey &amp;&amp; (pressedKey == &quot;c&quot; || pressedKey == &quot;u&quot;)) {
+alert(options.alertText);
+return false;
+}
+});
+});
 
 // REMOVE M=1
 var uri = window.location.toString();
@@ -94,7 +66,7 @@ var clean_uri = uri.substring(0, uri.indexOf("%3D%3D"));
 window.history.replaceState({}, document.title, clean_uri);
 }
 var uri = window.location.toString();
-if (uri.indexOf("&m=1","&m=1") > 0) {
+if (uri.indexOf("&amp;m=1","&amp;m=1") > 0) {
 var clean_uri = uri.substring(0, uri.indexOf("&m=1"));
 window.history.replaceState({}, document.title, clean_uri);
 }
